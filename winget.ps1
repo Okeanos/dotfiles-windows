b5b230111faf4f873d464ad552537dccef62d623
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2022-11-01 14:02:20 +0100

    improve and clean up winget installation script

--- winget.ps1
+++ winget.ps1
@@ -3,36 +3,33 @@
 # https://github.com/microsoft/winget-pkgs
 
 # TODO improve/simplify winget invocations
-winget install --id Git.Git --override "/VERYSILENT /NOCANCEL /LOADINF=$PSScriptRoot\winget_git.ini"
-winget install --id Google.Chrome --override "/quiet"
+winget install --id "Git.Git" --override "/VERYSILENT /NOCANCEL /LOADINF=$PSScriptRoot\winget_git.ini"
+winget install --id "Google.Chrome" --override "/quiet"
 # https://docs.docker.com/desktop/windows/install/#install-from-the-command-line
-winget install --id Docker.DockerDesktop
+winget install --id "Docker.DockerDesktop"
 # https://support.mozilla.org/en-US/kb/deploy-firefox-msi-installers
-winget install --id Mozilla.Firefox --override "OPTIONAL_EXTENSIONS=false /quiet"
-winget install --id Microsoft.VisualStudioCode --override "--silent"
+winget install --id "Mozilla.Firefox" --override "OPTIONAL_EXTENSIONS=false /quiet"
+winget install --id "Microsoft.VisualStudioCode" --override "--silent"
 
 foreach ($package in @(
-	7zip.7zip
-	Adobe.Acrobat.Reader.64-bit
-	#AgileBits.1Password
-	#ahmetb.kubectx
-	#ahmetb.kubens
-	#Apache.Maven
-	Atlassian.Sourcetree
-	#DominikReichl.KeePass
-	EclipseAdoptium.Temurin.17
-	Golang.Go
-	Jetbrains.Toolbox
-	#johanhaleby.kubetail
-	KeePassXCTeam.KeePassXC
-	Kubernetes.kubectl
-	Microsoft.WindowsTerminal
-	MikeFarah.yq
-	#Notepad++.Notepad++
-	OpenJS.NodeJS
-	Starship.Starship
-	stedolan.jq
-	Yarn.Yarn
+	"7zip.7zip"
+	"Adobe.Acrobat.Reader.64-bit"
+	#"ahmetb.kubectx"
+	#"ahmetb.kubens"
+	#"Apache.Maven"
+	"Atlassian.Sourcetree"
+	"EclipseAdoptium.Temurin.17.JDK"
+	"GoLang.Go.1"
+	"Jetbrains.Toolbox"
+	#"johanhaleby.kubetail"
+	"KeePassXCTeam.KeePassXC"
+	"Kubernetes.kubectl"
+	"Microsoft.WindowsTerminal"
+	"MikeFarah.yq"
+	"OpenJS.NodeJS"
+	"Starship.Starship"
+	"stedolan.jq"
+	"Yarn.Yarn"
 	)
 ) {
 	winget install --id $package
