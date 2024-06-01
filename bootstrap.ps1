64676f2c9d5d4eeb1c8aff8b8dd04c842232e9e1
Nikolas Grottendieck <git@nikolasgrottendieck.com>
2024-06-01 15:47:26 +0200

    add missing link to bat themes dir

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -88,6 +88,7 @@ Function DoIt
 
 	Write-Host "Linking files"
 	LinkFiles "$( $PSScriptRoot )\stow\bat\" "$( $ENV:AppData )\bat\"
+	LinkFiles "$( $PSScriptRoot )\stow\bat\themes\" "$( $ENV:AppData )\bat\themes\"
 	LinkFiles "$( $PSScriptRoot )\stow\maven\.m2\" "$( $ENV:UserProfile )\.m2\"
 	LinkFiles "$( $PSScriptRoot )\stow\misc\" "$( $ENV:UserProfile )\"
 	LinkFiles "$( $PSScriptRoot )\stow\shell\" "$( $ENV:UserProfile )\"
